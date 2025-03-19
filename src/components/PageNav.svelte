a131c2c75028d724ac2a43d2bbeb4435ecfaf77c
Spider-Frog <germanspiderfrog@gmail.com>
2025-03-19 17:42:43 +0100

    Project setup (wrong setting oops)

---
+++ src/components/PageNav.svelte
@@ -0,0 +1,6 @@
+<aside>
+  <h1>MailBurner</h1>
+  <nav class="bg-red-500">
+    test
+  </nav>
+</aside>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+  import PageNav from "../components/PageNav.svelte";
+	let { children } = $props();
+</script>
+
+<div>
+  <PageNav />
+  <main>
+    {@render children()}
+  </main>
+</div>
