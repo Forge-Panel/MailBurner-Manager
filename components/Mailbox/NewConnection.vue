d6ad39de622d14bb6b23f17c544a8d78afd81d61
SpiderFrog <germanspiderfrog@gmail.com>
2025-05-26 02:16:06 +0200

    Added graphics to new connection mailbox manager

--- components/Mailbox/NewConnection.vue
+++ components/Mailbox/NewConnection.vue
@@ -33,37 +33,73 @@ const mailboxProviders = computed<mailboxProvider[]>(() => [
     key: 'imap',
     name: 'IMAP',
   }
-])
+]);
+
+const showDialog = ref(false)
+const step = ref(1)
+
+function previousStep() {
+  step.value -= 1
+  if (step.value <= 0) {
+    step.value = 1
+  }
+}
+
+function show() {
+  showDialog.value = true
+  step.value = 0;
+}
+
+function close() {
+  showDialog.value = false
+  step.value = 0;
+}
 </script>
 
 <template>
-  <v-dialog>
-    <template #activator="{ props: activatorProps }">
+  <v-dialog v-model="showDialog">
+    <template #activator>
       <v-btn
           class="mb-8"
-          color="blue-lighten-1"
+          color="primary"
           prepend-icon="i-mdi:plus"
-          v-bind="activatorProps"
           variant="flat"
           :text="$t('mailboxes.btnNewConnection')"
+          @click="show()"
       />
     </template>
-    <template #default="{ isActive }">
-      <v-card max-width="1000" class="mx-auto p-0" title="Choose prodiver">
-        <v-card-text class="d-flex flex-wrap justify-space-between">
-          <v-card v-for="mailboxProvider in mailboxProviders" :key="mailboxProvider.key" class="mx">
-            <v-img
-                color="surface-variant"
-                class="mx-auto my-2"
-                width="250"
-                :src="`/img/mail_providers/${mailboxProvider.key}.jpg`"
-                cover
-            />
-            <v-card-actions>
-              <v-btn>test</v-btn>
-            </v-card-actions>
-          </v-card>
+    <template #default>
+      <v-card max-width="1000" class="mx-auto" title="New Mail Connection">
+        <v-card-text class="d-flex flex-wrap justify-space-between px-0">
+          <v-stepper v-model="step" :items="['Choose provider', 'Connect', 'Confirm']" flat hide-actions>
+            <template #item.1>
+              <div class="d-flex flex-wrap justify-space-between">
+                <v-card v-for="mailboxProvider in mailboxProviders" :key="mailboxProvider.key" class="mx my-4" @click="step = 2">
+                  <v-img
+                      color="surface-variant"
+                      class="mx-auto"
+                      width="250"
+                      :src="`/img/mail_providers/${mailboxProvider.key}.jpg`"
+                      cover
+                      @dragstart.prevent
+                  />
+                </v-card>
+              </div>
+            </template>
+
+            <template #item.2>
+              <v-card title="Step Two" flat>...</v-card>
+            </template>
+
+            <template #item.3>
+              <v-card title="Step Three" flat>...</v-card>
+            </template>
+          </v-stepper>
         </v-card-text>
+        <v-card-actions>
+          <v-btn text="Cancel" @click="close()" />
+          <v-btn :text="$t('$vuetify.stepper.prev')" class="mr-auto" :disabled="step <= 1" @click="previousStep()" />
+        </v-card-actions>
       </v-card>
     </template>
   </v-dialog>
--- server/plugins/db.ts
+++ server/plugins/db.ts
@@ -1,5 +1,5 @@
 import sequelize from '../lib/db';
 
 export default defineNitroPlugin(async (nitroApp) => {
-  await sequelize.sync()
+  // await sequelize.sync()
 })
